20e1e12b9d09117a01ca4a941aa23d5a9ce83de3
Brian Howell <brianhowell@pm.me>
2024-02-08 01:47:07 -0500

    wip placeholder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Headshot from "../../assets/images/headshot.jpeg";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img src={Headshot} alt="brnwll headshot lookin fly" />
+      <br />
+      <p>
+        <b>wip:</b> coming soon
+      </p>
+    </div>
+  );
 }
 
 export default App;
